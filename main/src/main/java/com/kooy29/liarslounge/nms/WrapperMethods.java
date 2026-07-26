670c22aebc37486297300e1c9ee426c667a1e971
aarcorrea <aaroncorrea7761@gmail.com>
2026-07-26 16:50:43 +0530

    fixed unmounting of player just before game starts

--- main/src/main/java/com/kooy29/liarslounge/nms/WrapperMethods.java
+++ main/src/main/java/com/kooy29/liarslounge/nms/WrapperMethods.java
@@ -1,7 +1,6 @@
 package com.kooy29.liarslounge.nms;
 
 import com.kooy29.liarslounge.LiarsLounge;
-import com.kooy29.liarslounge.api.arena.GameState;
 import com.kooy29.liarslounge.api.arena.IArena;
 import com.kooy29.liarslounge.api.nms.IWrapperMethods;
 import com.kooy29.liarslounge.utils.ExtraUtil;
@@ -14,8 +13,7 @@ public class WrapperMethods implements IWrapperMethods {
     public boolean canPlayerUnmount(Player player) {
         if (!LiarsLounge.getInstance().getArenaManager().isPlayerInArena(player)) return true;
         IArena arena = LiarsLounge.getInstance().getArenaManager().getArenaByPlayer(player);
-        return arena.isSpectator(player)
-                || arena.getGameState() != GameState.PLAYING || arena.isEnding();
+        return arena.isSpectator(player) || arena.isEnding();
     }
 
     @Override
